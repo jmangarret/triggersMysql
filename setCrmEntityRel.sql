9771c6135185a2335b6ec6960ce43c39df9d5c9d
Jonathan <informatica@tuagencia24.com>
2016-01-13 17:14:13 -0430

    correcion trigger setCrmEntityRel y campos listview de boletos

--- setCrmEntityRel.sql
+++ setCrmEntityRel.sql
@@ -3,13 +3,17 @@ DELIMITER |
 CREATE DEFINER=`root`@`localhost` PROCEDURE `setCrmEntityRel`(
 	IN _modulo VARCHAR(250), 
 	IN _modulorel VARCHAR(250), 
-	IN _crmid INT,
-	IN _crmidrel INT	
+	IN _crmid VARCHAR(25), 
+	IN _crmidrel VARCHAR(25)	
 	)
 BEGIN 
 DECLARE totBoletosBs DOUBLE(25,2);
 DECLARE totBoletosDol DOUBLE(25,2);
-IF (modulo="RegistroDeVenta" AND modulorel="Localizadores") THEN
+SET @mod1=_modulo;
+SET @mod2=_modulorel;
+SET @id1=_crmid;
+SET @id2=_crmidrel;
+IF (_modulo="RegistroDeVentas" AND _modulorel="Localizadores") THEN
 	SET totBoletosBs	=(SELECT SUM(totalboletos) FROM vtiger_boletos WHERE currency="VEF" AND localizadorid=_crmidrel);	
 	SET totBoletosDol	=(SELECT SUM(totalboletos) FROM vtiger_boletos WHERE currency="USD" AND localizadorid=_crmidrel);	
 	UPDATE vtiger_registrodeventas 
--- trigger_registrodeventas_loc.sql
+++ trigger_registrodeventas_loc.sql
@@ -2,6 +2,6 @@ DROP TRIGGER IF EXISTS registrodeventas_loc_insert;
 DELIMITER |
 CREATE TRIGGER registrodeventas_loc_insert AFTER INSERT ON vtiger_crmentityrel
 FOR EACH ROW BEGIN 	
-	CALL vtigercrm600.setCrmEntityRel(NEW.module,NEW.relmodule,NEW.crmid,NEW.relcrmid);	
+	CALL setCrmEntityRel(NEW.module,NEW.relmodule,NEW.crmid,NEW.relcrmid);	
 END |
 DELIMITER ;
