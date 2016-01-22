e9d207263c6063ea7ecb268b67f1d6db947d88cb
webmaster <webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.com>
2016-01-22 10:53:47 -0430

    triggers y sp ibip crm

--- getCrmUser.sql
+++ getCrmUser.sql
@@ -1,12 +1,12 @@
 DROP PROCEDURE IF EXISTS `getCrmUser`;
 DELIMITER |
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getCrmUser`(
-	IN ced VARCHAR(20)
-	)
+ IN ced VARCHAR(20)
+ )
 BEGIN 
 SET @iduser=0;
 IF ced<>'' THEN
-	SET @iduser=(SELECT id FROM vtiger_users WHERE cedula =ced or agenteid like concat('%',ced,'%')); 
+ SET @iduser=(SELECT id FROM vtiger_users WHERE cedula =ced or agenteid like concat('%',ced,'%')); 
 END IF;
 END |
 DELIMITER ;
