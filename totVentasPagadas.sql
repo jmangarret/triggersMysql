6b083e2f013a128f9d9e30f99ae1adfe3c450aac
Jonathan <informatica@tuagencia24.com>
2016-01-29 17:05:47 -0430

    Correcion SP totVentasPagadas, retornaba valores NULL

--- totVentasPagadas.sql
+++ totVentasPagadas.sql
@@ -2,19 +2,53 @@ DROP PROCEDURE IF EXISTS totVentasPagadas;
 DELIMITER |
 CREATE DEFINER=`root`@`localhost` PROCEDURE `totVentasPagadas`(
 	IN _pagoId INT,
-	IN _ventaId
-	)
+	IN _ventaId INT)
 BEGIN 
 DECLARE _cambio DOUBLE(25,2); 
-DECLARE _status VARCHAR(250); 
+DECLARE _status VARCHAR(100); 
 DECLARE _totVentaDol DOUBLE(25,2); 
 DECLARE _totVentaBs DOUBLE(25,2); 
 DECLARE _totPagosDol DOUBLE(25,2);	
 DECLARE _totPagosBs DOUBLE(25,2);
 DECLARE _totPendBs DOUBLE(25,2);
-DECLARE _totPendDol DOUBLE(25,2)
-SELECT cf_1621, cf_881 INTO _cambio, _status FROM vtiger_registrodeventascf WHERE registrodeventasid = _ventaId;
+DECLARE _totPendDol DOUBLE(25,2);
+DECLARE _totDolEnBs DOUBLE(25,2);
+SELECT cf_881 , cf_1621 INTO _cambio, _status FROM vtiger_registrodeventascf WHERE registrodeventasid = _ventaId;
+IF (ISNULL(_cambio)) THEN 
+	SET _cambio=1;
+END IF;
 
+SELECT 	IF(ISNULL(totalventabs),0,totalventabs), 
+		IF(ISNULL(totalventadolares),0,totalventadolares) 
+		INTO _totVentaBs,_totVentaDol  
+		FROM vtiger_registrodeventas 
+		WHERE registrodeventasid = _ventaId;
+
+SET _totPagosDol=(SELECT IF(ISNULL(SUM(amount)),0,SUM(amount)) AS pagoDolares FROM vtiger_registrodepagos WHERE currency="USD" AND registrodeventasid = _ventaId);
+SET _totPagosBs	=(SELECT IF(ISNULL(SUM(amount)),0,SUM(amount)) AS PagoBs FROM vtiger_registrodepagos WHERE currency="VEF" AND registrodeventasid =_ventaId);
+
+SET _totPendBs = _totVentaBs - _totPagosBs;
+SET _totPendDol = _totVentaDol - _totPagosDol;
+
+IF (_totPagosBs>_totVentaBs AND _totPendDol>=0 AND (_totVentaBs>0 OR _totVentaDol>0)) THEN
+	SET _totDolEnBs = (_totPagosBs - _totVentaBs) / _cambio;
+	SET _totPendDol = _totVentaDol - _totPagosDol - _totDolEnBs;
+	SET _totPendBs = _totPendDol * _cambio;
+
+END IF;
+
+UPDATE vtiger_registrodeventas SET totalpagadobs=_totPagosBs, totalpagadodolares= _totPagosDol, totalpendientebs=_totPendBs, totalpendientedolares=_totPendDol 
+WHERE registrodeventasid =_ventaId;
+
+IF (_status<>"Procesada") THEN
+	IF (_totPendDol<=0 AND _totPendBs<=0) THEN
+		UPDATE vtiger_registrodeventascf SET cf_1621="No Procesada" WHERE registrodeventasid = _ventaId;
+	ELSE
+		IF (_status="No Procesada" OR _status="Pagada") THEN
+			UPDATE vtiger_registrodeventascf SET cf_1621="Pendiente de Pago" WHERE registrodeventasid = _ventaId;
+		END IF;
+	END IF;
+END IF;
 
 END |
 DELIMITER ;
