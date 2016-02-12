fc9fd5f76e0695d6c4472c8b91fbfddec00538cd
webmaster <webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.com>
2016-02-12 11:48:22 -0430

    Nuevo archivo de setTicketId

---
+++ setTicketId.sql
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------------------------------------------------*/
+/*TRIGGER EN LA BASE DE DATOS DE "REGISTRO_BOLETOS"*/
+DROP TRIGGER IF EXISTS setTicketId;
+DELIMITER //
+CREATE TRIGGER setTicketId
+AFTER INSERT ON registro_boletos.boletos
+FOR EACH ROW BEGIN
+DECLARE lastInsert INT;
+
+SET lastInsert = 0;
+
+SET lastInsert = (SELECT max(id) FROM registro_boletos.boletos);
+call vtigercrm600.setTickets(lastInsert);
+END//
+DELIMITER ;
+
+/*---------------------------------------------------*/
