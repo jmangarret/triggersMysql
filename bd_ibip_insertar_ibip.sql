f636d26fab84c3b2e33db1f9fa119057bbf2416e
webmaster <webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.com>
2016-02-25 17:03:08 -0430

    Modificación del Trigger insertar_ibip para el nuevo campo de impuestos totales

--- bd_ibip_insertar_ibip.sql
+++ bd_ibip_insertar_ibip.sql
@@ -12,11 +12,11 @@ FOR EACH ROW BEGIN
 	############################
 	INSERT INTO registro_boletos.boletos (id_xml,localizador, currency, fee_percentage, fee, total_amount,
 	montobase, coupon_status, passenger, sistemagds, emittedDate, creationDate, departureDate, 
-	ticketNumber, airlineID, YN_tax, status_emission, nombre_asesora, ID_asesora, nombre_satelite, 
+	ticketNumber, airlineID, YN_tax, total_tax, status_emission, nombre_asesora, ID_asesora, nombre_satelite, 
 	ID_satelite, tipo_vuelo, itinerary, method_payment)
 	VALUES (NEW.fileSourcefileName,NEW.pnrLocator, NEW.bookingCurrency, NEW.fareCommissionPercent, NEW.fareCommission, NEW.totalPrice, NEW.price, NEW.voidStatus,
-	NEW.passenger, NEW.gdsName, NEW.itemDate, NEW.pnrCreationDate, NEW.departureDate, NEW.ticketNumber, CONCAT(NEW.airlineid,'/',NEW.airlinedescription), NEW.PriceVAT, _emittedStatus, 
-	NEW.ticketingAgentname, NEW.ticketingAgentid, NEW.bookingAgentname, NEW.bookingAgentid, NEW.marketPlace, NEW.itinerary,@find_paymentmethod);
+	NEW.passenger, NEW.gdsName, NEW.itemDate, NEW.pnrCreationDate, NEW.departureDate, NEW.ticketNumber, CONCAT(NEW.airlineid,'/',NEW.airlinedescription), NEW.PriceVAT, NEW.priceVAT+NEW.otherTax,
+    _emittedStatus, NEW.ticketingAgentname, NEW.ticketingAgentid, NEW.bookingAgentname, NEW.bookingAgentid, NEW.marketPlace, NEW.itinerary,@find_paymentmethod);
 
 END |
 DELIMITER ;
