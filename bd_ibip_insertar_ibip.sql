d08d79046bd0abc40e8005a001948a134516eb9f
Jonathan <informatica@tuagencia24.com>
2016-02-26 15:41:30 -0430

    Concat num de boleto aerolinea, rename getPaymentMethod

--- bd_ibip_insertar_ibip.sql
+++ bd_ibip_insertar_ibip.sql
@@ -9,13 +9,12 @@ FOR EACH ROW BEGIN
 		SET _emittedStatus=(SELECT emittedStatus FROM ifm_pnrairsegment WHERE ticketNumber=NEW.ticketNumber);
 	END IF;
 	CALL getPaymentMethod(NEW.pnrLocator);
-	############################
 	INSERT INTO registro_boletos.boletos (id_xml,localizador, currency, fee_percentage, fee, total_amount,
 	montobase, coupon_status, passenger, sistemagds, emittedDate, creationDate, departureDate, 
 	ticketNumber, airlineID, YN_tax, status_emission, nombre_asesora, ID_asesora, nombre_satelite, 
 	ID_satelite, tipo_vuelo, itinerary, method_payment)
 	VALUES (NEW.fileSourcefileName,NEW.pnrLocator, NEW.bookingCurrency, NEW.fareCommissionPercent, NEW.fareCommission, NEW.totalPrice, NEW.price, NEW.voidStatus,
-	NEW.passenger, NEW.gdsName, NEW.itemDate, NEW.pnrCreationDate, NEW.departureDate, NEW.ticketNumber, CONCAT(NEW.airlineid,'/',NEW.airlinedescription), NEW.PriceVAT, _emittedStatus, 
+	NEW.passenger, NEW.gdsName, NEW.itemDate, NEW.pnrCreationDate, NEW.departureDate, CONCAT(NEW.airlineiataNumericCode,'-',NEW.ticketNumber), CONCAT(NEW.airlineid,'/',NEW.airlinedescription), NEW.PriceVAT, _emittedStatus, 
 	NEW.ticketingAgentname, NEW.ticketingAgentid, NEW.bookingAgentname, NEW.bookingAgentid, NEW.marketPlace, NEW.itinerary,@find_paymentmethod);
 
 END |
