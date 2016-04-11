6b971c928fc3ec3db6be90565d36f94dba7ad12d
webmaster <webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.com>
2016-04-11 09:53:16 -0430

    Agregado en la carpeta script vtiger los campos satelites y usuarios en Firmas

---
+++ scripts vtiger/addFieldNombreAsociado.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', true);
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+require_once('vtlib/Vtiger/Module.php');
+include_once('vtlib/Vtiger/Utils.php');
+include_once('vtlib/Vtiger/Menu.php');
+require_once('vtlib/Vtiger/Block.php');
+require_once('vtlib/Vtiger/Field.php');
+$Vtiger_Utils_Log = true;
+$users=Vtiger_Module::getInstance('Terminales');
+$block = Vtiger_Block::getInstance('Información General',$users);
+
+$fieldInstance = new Vtiger_Field();
+$fieldInstance->name = 'contactoid'; //Usually matches column name
+$fieldInstance->table = 'vtiger_terminales';
+$fieldInstance->column = 'contactoid'; //Must be lower case
+$fieldInstance->label = 'Nombre asociado'; //Upper case preceeded by LBL_
+$fieldInstance->columntype = 'INT'; //
+$fieldInstance->uitype = 10; //Campo check
+$fieldInstance->typeofdata = 'V~O'; //V=Varchar?, M=Mandatory, O=Optional
+$block->addField($fieldInstance);
+$fieldInstance->setRelatedModules(Array('Contacts'));
+
+
+$block->save($users);
+$users->initWebservice();
+echo 'Code successfully executed';
+?>
