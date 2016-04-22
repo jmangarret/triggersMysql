96f1d4fc0178f1e4b9ae55b8de96bf782517cce7
webmaster <webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.comgit config --global user.email webmaster@tuagencia24.com>
2016-04-22 12:15:46 -0430

    Modificado archivo sp_validar_firmas.sql para mejorar las validaciones por si ya existe una firma en otro contacto o usuario

--- sp_validar_firmas.sql
+++ sp_validar_firmas.sql
@@ -6,6 +6,7 @@ IN id_contactid INT)
 BEGIN 
 DECLARE find_firmas_satelite VARCHAR(100);
 DECLARE find_agenteid VARCHAR(100);
+DECLARE find_old_contactid INT;
 
 SELECT firmas_satelite INTO find_firmas_satelite FROM vtiger_contactdetails WHERE contactid=id_contactid;
 
@@ -14,14 +15,32 @@ IF ROW_COUNT()=0 THEN
 	SELECT agenteid INTO find_agenteid FROM vtiger_users WHERE id=id_contactid;
     
     IF isnull(find_agenteid) or (find_agenteid="") THEN
-		UPDATE vtiger_users SET agenteid=CONCAT(" |##| ", firma) WHERE id=id_contactid;
+		SELECT id INTO find_old_contactid FROM vtiger_users WHERE agenteid LIKE CONCAT("%",firma,"%");
+		IF ROW_COUNT()>0 THEN
+			UPDATE vtiger_users SET agenteid=REPLACE(agenteid, CONCAT(firma,' |##|'),'') WHERE id=find_old_contactid;
+		END IF;
+		UPDATE vtiger_users SET agenteid=CONCAT(firma, " |##| ") WHERE id=id_contactid;
 	ELSE
-		UPDATE vtiger_users SET agenteid=CONCAT(agenteid, " |##| ", firma) WHERE id=id_contactid;
+		SELECT id INTO find_old_contactid FROM vtiger_users WHERE agenteid LIKE CONCAT("%",firma,"%");
+		IF ROW_COUNT()>0 THEN
+			UPDATE vtiger_users SET agenteid=REPLACE(agenteid, CONCAT(firma,' |##|'),'') WHERE id=find_old_contactid;
+		END IF;
+		UPDATE vtiger_users SET agenteid=CONCAT(agenteid, firma, " |##| ") WHERE id=id_contactid;
     END IF;
 ELSEIF isnull(find_firmas_satelite) or (find_firmas_satelite="") THEN
-	UPDATE vtiger_contactdetails SET firmas_satelite=CONCAT(" |##| ", firma) WHERE contactid=id_contactid;
+
+	SELECT contactid INTO find_old_contactid FROM vtiger_contactdetails WHERE firmas_satelite LIKE CONCAT("%",firma,"%");
+    IF ROW_COUNT()>0 THEN
+		UPDATE vtiger_contactdetails SET firmas_satelite=REPLACE(firmas_satelite, CONCAT(firma,' |##|'),'') WHERE contactid=find_old_contactid;
+    END IF;
+    UPDATE vtiger_contactdetails SET firmas_satelite=CONCAT(firma, " |##| ") WHERE contactid=id_contactid;
+    
 ELSE
-	UPDATE vtiger_contactdetails SET firmas_satelite=CONCAT(firmas_satelite," |##| ", firma) WHERE contactid=id_contactid;
+	SELECT contactid INTO find_old_contactid FROM vtiger_contactdetails WHERE firmas_satelite LIKE CONCAT("%",firma,"%");
+    IF ROW_COUNT()>0 THEN
+		UPDATE vtiger_contactdetails SET firmas_satelite=REPLACE(firmas_satelite, CONCAT(firma,' |##|'),'') WHERE contactid=find_old_contactid;
+    END IF;
+	UPDATE vtiger_contactdetails SET firmas_satelite=CONCAT(firmas_satelite, firma, " |##| ") WHERE contactid=id_contactid;
 END IF;
 
 END|
