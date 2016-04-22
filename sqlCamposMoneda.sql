76b6caa0a6d402eabbdcf038d68f92e3268959a2
Jonathan <informatica@tuagencia24.com>
2016-04-22 12:21:12 -0430

    SQL para campos tipo moneda en todos los modulos

---
+++ sqlCamposMoneda.sql
@@ -0,0 +1,22 @@
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='fee' AND `vtiger_field`.`tablename` ='vtiger_boletos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='amount' AND `vtiger_field`.`tablename` ='vtiger_boletos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalboletos' AND `vtiger_field`.`tablename` ='vtiger_boletos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='monto_base' AND `vtiger_field`.`tablename` ='vtiger_boletos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='comision_sat' AND `vtiger_field`.`tablename` ='vtiger_boletos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='fee_airline' AND `vtiger_field`.`tablename` ='vtiger_boletos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='yn_tax' AND `vtiger_field`.`tablename` ='vtiger_boletos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='total_tax' AND `vtiger_field`.`tablename` ='vtiger_boletos';
+
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalloc' AND `vtiger_field`.`tablename` ='vtiger_localizadores';
+
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalventabs' AND `vtiger_field`.`tablename` ='vtiger_registrodeventas';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalventadolares' AND `vtiger_field`.`tablename` ='vtiger_registrodeventas';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalpagadobs' AND `vtiger_field`.`tablename` ='vtiger_registrodeventas';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalpagadodolares' AND `vtiger_field`.`tablename` ='vtiger_registrodeventas';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalpendientebs' AND `vtiger_field`.`tablename` ='vtiger_registrodeventas';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalpendientedolares' AND `vtiger_field`.`tablename` ='vtiger_registrodeventas';
+
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='amount' AND `vtiger_field`.`tablename` ='vtiger_registrodepagos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalpagado' AND `vtiger_field`.`tablename` ='vtiger_registrodepagos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='pendientepago' AND `vtiger_field`.`tablename` ='vtiger_registrodepagos';
+UPDATE  `vtiger_field` SET  `uitype` =  '71' WHERE  `vtiger_field`.`fieldname` ='totalventa' AND `vtiger_field`.`tablename` ='vtiger_registrodepagos';
