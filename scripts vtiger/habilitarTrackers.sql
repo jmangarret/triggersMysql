f09c21a16caa351950f549b45ea6caf5267fd8cb
icomp <icompsoluciones@gmail.com>
2016-08-09 17:37:00 -0430

    Vista usuarios para PHP Report Maker, Status soto, SQl trackers

---
+++ scripts vtiger/habilitarTrackers.sql
@@ -0,0 +1,11 @@
+/*Habilitar trackers id modulo*/
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('60', '1');
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('61', '1');
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('62', '1');
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('63', '1');
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('64', '1');
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('65', '1');
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('66', '1');
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('67', '1');
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('68', '1');
+INSERT INTO `vtiger_modtracker_tabs` (`tabid`, `visible`) VALUES ('69', '1');
