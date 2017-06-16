f93317dc659c91b1fd343bd8dd2be13ffdcf722a
Jonathan <informatica@tuagencia24.com>
2017-06-16 17:07:36 -0430

    Fix spam ticket, sql locs duplicados, urls webservices qry locs

--- trigger_bloqueo_spam_joomla.sql
+++ trigger_bloqueo_spam_joomla.sql
@@ -3,10 +3,14 @@ DELIMITER |
 CREATE TRIGGER `bloqueo_spam` BEFORE INSERT ON `ost_ticket`
  FOR EACH ROW 
  BEGIN
-   IF NEW.ip_address LIKE CONCAT('188.143.232.','%') THEN
-    SET NEW.ticket_id=NULL;
-    SET NEW.user_id=NULL;
-    SET NEW.staff_id=NULL;
+   IF NEW.ip_address LIKE CONCAT('188.143.232.','%') 
+   		OR NEW.ip_address LIKE CONCAT('178.159.37.15','%') 
+   		OR NEW.ip_address LIKE CONCAT('64.202.160.236','%') 
+   		OR NEW.ip_address LIKE CONCAT('216.69.191.97','%') 
+   THEN
+    	SET NEW.ticket_id=NULL;
+    	SET NEW.user_id=NULL;
+    	SET NEW.staff_id=NULL;    
    END IF;
   END |
 DELIMITER ;
