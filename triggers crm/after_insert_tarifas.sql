8b91ada1e50c1f8e1e6483c1386906c1bddea574
Jonathan-Pc <jmangarret@gmail.com>
2017-08-06 22:08:37 -0400

    Correcion de trigger historico e inicializacion de data

--- triggers crm/after_insert_tarifas.sql
+++ triggers crm/after_insert_tarifas.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER after_insert_tarifas AFTER INSERT ON vtiger_tarifas
   FOR EACH ROW BEGIN 
 		INSERT INTO vtiger_tarifas_historico SET tarifasid = new.tarifasid, fecha= CURRENT_DATE, valor= new.valor_base ;
   	END;|
-DELIMITER;
+DELIMITER ;
   	
 /*trigger que inserta en la tabla historica (vtiger_tarifas_historico) una vez se ha modificado en la tabla vtiger_tarifas*/
 DROP TRIGGER IF EXISTS `after_update_tarifas`;
@@ -14,4 +14,4 @@ CREATE TRIGGER after_update_tarifas AFTER UPDATE ON vtiger_tarifas
   FOR EACH ROW BEGIN 
 		UPDATE vtiger_tarifas_historico SET tarifasid = old.tarifasid, fecha= CURRENT_DATE, valor= new.valor_base WHERE tarifasid=old.tarifasid;
   	END;|
-DELIMITER;
+DELIMITER ;
